8ddec211084c69735b43b8d4c2b8bb4dca8976d0
MohammedGhonmi <MohammedGhonmi@users.noreply.github.com>
2020-02-16 15:53:37 +0200

    removing add

--- SW.DeeBee.UnitTests/UnitTest1.cs
+++ SW.DeeBee.UnitTests/UnitTest1.cs
@@ -13,13 +13,13 @@ namespace SW.DeeBee.UnitTests
         [TestMethod]
         async public Task TestMethod1()
         {
-            var bag = new Bag
-            {
-                Number ="65456456",
-                Description = "test bag",
-                Entity = "XYZ",
-                SampleDate = DateTime.UtcNow
-            };
+            //var bag = new Bag
+            //{
+            //    Number ="65456456",
+            //    Description = "test bag",
+            //    Entity = "XYZ",
+            //    SampleDate = DateTime.UtcNow
+            //};
 
             //using (var  connectionHost = new ConnectionHost(new DeeBeeOptions()
             //{
@@ -35,11 +35,11 @@ namespace SW.DeeBee.UnitTests
             using (var connection = new MySqlConnection("Server=mysql-s9-do-user-6997732-0.db.ondigitalocean.com;Port=25060;Database=mailboxdb;User=doadmin;Password=pwpxz6xcmxxq9tlv;sslmode=none;"))
             {
                 await connection.OpenAsync();
-                await connection.Add(bag);
+                //await connection.Add(bag);
 
-                bag.Description = "ttt";
+                //bag.Description = "ttt";
 
-                await connection.Update(bag);
+                //await connection.Update(bag);
                 var bags = await connection.All<Bag>();
                 var bag1 = await connection.One<Bag>(20);
 
