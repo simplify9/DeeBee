f758437856a458fc132e411895cce7bff28745c5
Shaheen Sarafa <shaheen@simplify9.com>
2020-06-25 14:08:39 +0300

    added create and drop table

---
+++ SW.DeeBee/SqlTypeInformation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SW.DeeBee
+{
+    public class SqlTypeInformation
+    {
+        private string sqlType;
+        public string SqlType { get => sqlType; set { sqlType = value; } }
+        public bool IsUnique { get; set; }
+    }
+}
