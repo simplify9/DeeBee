697c5fa0c6f8cf71e2902d22936cd61a1f68db9e
MohammedGhonmi <MohammedGhonmi@users.noreply.github.com>
2020-08-26 16:15:06 +0300

    .

--- SW.DeeBee/DbConnectionExtensions.cs
+++ SW.DeeBee/DbConnectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using SW.PrimitiveTypes;
+﻿using SW.ObjectConversion;
+using SW.PrimitiveTypes;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -18,36 +19,7 @@ namespace SW.DeeBee
     {
         private static string SqlProviderName(this DbConnection connection) => connection.GetType().FullName;
         
-        private static Type GetNullableType(Type type)
-        {
-            if (type == typeof(string)) return typeof(string);
-            if (type == typeof(object)) return typeof(object);
-            return Nullable.GetUnderlyingType(type);
-        }
-        public static object ConvertValueToType(this object value, Type type)
-        {
-
-            bool typeNullable = (GetNullableType(type) == null) ? false : true;
-
-            if (value is null && typeNullable) return null;
-
-            else if (value is null) return Activator.CreateInstance(type);
-
-            if (value.GetType() == type) return value;
-            if (type.IsAssignableFrom(value.GetType())) return value;
-
-            var nakedType = GetNullableType(type);
-            if (nakedType != null)
-            {
-                if (string.IsNullOrWhiteSpace(value.ToString())) return null;
-
-                if (nakedType.IsEnum) return Enum.Parse(nakedType, value.ToString(), true);
-                return Convert.ChangeType(value, nakedType);
-            }
-
-            if (type.IsEnum) return Enum.Parse(type, value.ToString(), true);
-            return Convert.ChangeType(value, type);
-        }
+    
         internal const string MYSQL = "MySql.Data.MySqlClient.MySqlConnection";
         internal const string MSSQL = "System.Data.SqlClient.SqlConnection";
         internal const string SQLITE = "Microsoft.Data.Sqlite.SqliteConnection";
